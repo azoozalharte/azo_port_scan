e49fbda332a0083b46005baf511ffa523ff8c765
azoozalharte <zezo50115@protonmail.com>
2021-04-26 18:00:26 +0300

    ping the traget

--- azoScan.py
+++ azoScan.py
@@ -1,4 +1,16 @@
 import socket
+import os
+import platform
+
+
+def check_ping(target):
+    response = os.system(
+        "ping " + ("-n 1 " if platform.system().lower() == "windows" else "-c 1 ") + target)
+    if response == 0:
+        pingstatus = "Network Active"
+    else:
+        pingstatus = 'Network Error'
+    return pingstatus
 
 
 def repeat_scan(target, ports):
@@ -19,11 +31,17 @@ def port_scan(ipaddress, port):
 
 targets = input("[*] Enter Targets To Scan(split them by , If there any): ")
 ports = input(
-    '[*] Enter How Many Ports You Want 19To Scan: ')
+    '[*] Enter How Many Ports You Want To Scan: ')
 
 if ',' in targets:
     print('[*] Scaning Multiple Targets')
     for ip_addr in targets.split(','):
-        repeat_scan(ip_addr.strip(' '), int(ports))
-else:
+        if check_ping(ip_addr.strip(' ')) == "Network Active":
+            repeat_scan(ip_addr.strip(' '), int(ports))
+        else:
+            print('\n' +
+                  "[-] This Target is offline or out of your network: " + ip_addr.strip(' '))
+            pass
+elif check_ping(targets) == "Network Active":
+    print('[*] Scaning Single Target')
     repeat_scan(targets, int(ports))
